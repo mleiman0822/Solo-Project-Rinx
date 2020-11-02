fc1e0c82b8d5397ef6515f3400ead29cd8a46b8b
Matthew Leiman <matthewtleiman@gmail.com>
2020-11-02 14:43:21 -0600

    admin form alligned

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,9 +4,9 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import RinkMap from "../RinkMap/RinkMap";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
 import { Modal, Button } from "react-bootstrap";
 import "../App/App.css";
+import { Row, Col } from "react-bootstrap";
 
 class AdminForm extends Component {
   state = {
@@ -58,13 +58,17 @@ class AdminForm extends Component {
             Add A New Rink{" "}
           </Button>
         </div>
-        <Modal show={this.state.isOpen} onHide={this.closeModal}>
+        <Modal
+          className="modal"
+          show={this.state.isOpen}
+          onHide={this.closeModal}
+        >
           <Modal.Header closeButton>
             <Modal.Title style={{ justifyContent: "center" }}>
               Add A New Rink
             </Modal.Title>
           </Modal.Header>
-          <Modal.Body>
+          <Modal.Body className="modal-body">
             {" "}
             <body>
               <form
@@ -77,74 +81,114 @@ class AdminForm extends Component {
                   Latitude / Longitude points. After adding the map, the map
                   will auto-update but it will not auto-update to show all pins.
                 </p>
-                Name:{" "}
-                <input
-                  onChange={(e) => this.setState({ name: e.target.value })}
-                  value={this.state.name}
-                  required
-                />
-                {""}
-                Notes:{" "}
-                <input
-                  onChange={(e) => this.setState({ note: e.target.value })}
-                  value={this.state.note}
-                  required
-                />
+                <Row>
+                  <Col>
+                    Name:{" "}
+                    <input
+                      onChange={(e) => this.setState({ name: e.target.value })}
+                      value={this.state.name}
+                      required
+                    />
+                  </Col>
+                  <Col>
+                    {""}
+                    Notes:{" "}
+                    <input
+                      onChange={(e) => this.setState({ note: e.target.value })}
+                      value={this.state.note}
+                      required
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    Address:{" "}
+                    <input
+                      onChange={(e) =>
+                        this.setState({ address: e.target.value })
+                      }
+                      value={this.state.address}
+                      required
+                    />{" "}
+                  </Col>
+                  <Col>
+                    Image URL:{" "}
+                    <input
+                      onChange={(e) => this.setState({ image: e.target.value })}
+                      value={this.state.image}
+                      required
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    Status:{" "}
+                    <select
+                      onChange={(e) =>
+                        this.setState({ status: e.target.value })
+                      }
+                      value={this.state.status}
+                      required
+                    >
+                      <option value={3}>Useable</option>
+                      <option value={2}>Useable/Needs Maintenance</option>
+                      <option value={1}>Not Usable</option>
+                    </select>
+                  </Col>
+                  <Col>
+                    Indoor or Outdoor?:{" "}
+                    <input
+                      onChange={(e) =>
+                        this.setState({ indoor_or_outdoor: e.target.value })
+                      }
+                      value={this.state.indoor_or_outdoor}
+                      required
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    Latitude:{" "}
+                    <input
+                      onChange={(e) =>
+                        this.setState({ latitude: e.target.value })
+                      }
+                      value={this.state.latitude}
+                      required
+                    />
+                  </Col>
+
+                  <Col>
+                    Longitude:{" "}
+                    <input
+                      onChange={(e) =>
+                        this.setState({ longitude: e.target.value })
+                      }
+                      value={this.state.longitude}
+                      required
+                    />
+                  </Col>
+                </Row>
                 <br />
-                Address:{" "}
-                <input
-                  onChange={(e) => this.setState({ address: e.target.value })}
-                  value={this.state.address}
-                  required
-                />{" "}
-                Image URL:{" "}
-                <input
-                  onChange={(e) => this.setState({ image: e.target.value })}
-                  value={this.state.image}
-                  required
-                />
-                <br />
-                Status:{" "}
-                <select
-                  onChange={(e) => this.setState({ status: e.target.value })}
-                  value={this.state.status}
-                  required
-                >
-                  <option value={3}>Useable</option>
-                  <option value={2}>Useable/Needs Maintenance</option>
-                  <option value={1}>Not Usable</option>
-                </select>
-                <br />
-                Indoor or Outdoor?:{" "}
-                <input
-                  onChange={(e) =>
-                    this.setState({ indoor_or_outdoor: e.target.value })
-                  }
-                  value={this.state.indoor_or_outdoor}
-                  required
-                />
-                <br />
-                Latitude:{" "}
-                <input
-                  onChange={(e) => this.setState({ latitude: e.target.value })}
-                  value={this.state.latitude}
-                  required
-                />
-                Longitude:{" "}
-                <input
-                  onChange={(e) => this.setState({ longitude: e.target.value })}
-                  value={this.state.longitude}
-                  required
-                />
-                <div></div>
-                <Button type="submit" class="btn btn-secondary">
-                  Add Rink To Map
-                </Button>
+                <Row>
+                  <Col>
+                    <Button
+                      onSubmit={this.closeModal}
+                      type="submit"
+                      variant="primary w-100 text-center"
+                    >
+                      Add Rink To Map
+                    </Button>
+                  </Col>
+                </Row>
               </form>
             </body>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.closeModal}>
+            <Button
+              variant="secondary w-100 text-center"
+              onClick={this.closeModal}
+            >
               Close
             </Button>
           </Modal.Footer>
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -11,7 +11,7 @@ class UserPage extends Component {
     return (
       <div>
         <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
-        <p>Your ID is: {this.props.store.user.id}</p>
+        <p>Your User ID is: {this.props.store.user.id}</p>
         <LogOutButton className="log-in" />
         <br />
         <br />
