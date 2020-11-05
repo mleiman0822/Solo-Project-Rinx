48418d88c9c3f603612c8ff5acc52ac580a99379
Matthew Leiman <matthewtleiman@gmail.com>
2020-11-05 09:41:36 -0600

    updated logout button so it doesnt show unless user is logged in

--- src/components/LogOutButton/LogOutButton.js
+++ src/components/LogOutButton/LogOutButton.js
@@ -2,22 +2,27 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
 const LogOutButton = (props) => (
-  <Button
-    variant="dark"
-    // This button shows up in multiple locations and is styled differently
-    // because it's styled differently depending on where it is used, the className
-    // is passed to it from it's parents through React props
-    className={props.className}
-    onClick={() => props.dispatch({ type: "LOGOUT" })}
-  >
-    Log Out
-  </Button>
+  <div>
+    {props.store.user.id && (
+      <Button
+        variant="dark"
+        // This button shows up in multiple locations and is styled differently
+        // because it's styled differently depending on where it is used, the className
+        // is passed to it from it's parents through React props
+        className={props.className}
+        onClick={() => props.dispatch({ type: "LOGOUT" })}
+      >
+        Log Out
+      </Button>
+    )}
+  </div>
 );
 
 // This component doesn't need 'mapStateToProps'
 // because it doesn't care what the current state is.
 // No matter what the redux state is, this button will always be a log out button
 // this component still needs 'connect' though, because it is going to dispatch a redux action
-export default connect()(LogOutButton);
+export default connect(mapStoreToProps)(LogOutButton);
