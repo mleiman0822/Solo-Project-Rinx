88bdd4b090eb27ccaa8c0e2d6bdadd22d1817e25
Matthew Leiman <matthewtleiman@gmail.com>
2020-11-06 15:23:26 -0600

    readme stuff

--- database.sql
+++ database.sql
@@ -1,10 +1,66 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+
+"id" SERIAL PRIMARY KEY,
+"username" VARCHAR(300),
+"password" VARCHAR(300),
+"is_admin" BOOLEAN
+
 );
+
+CREATE TABLE "rinks" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(250) NOT NULL,
+    "note" VARCHAR(250) NOT NULL,
+    "image" VARCHAR(1500) NOT NULL,
+    "address" VARCHAR (1000) NOT NULL,
+    "status" INT,
+    "favorited" BOOLEAN,
+     "indoor_or_outdoor" VARCHAR(60) NOT NULL,
+	 "longitude" Decimal(10,6) NOT NULL,
+    "latitude" Decimal(10,6) NOT NULL
+);
+
+
+INSERT INTO "rinks" ("name", "note","image", "address", "status", "favorited","indoor_or_outdoor", "latitude", "longitude")
+VALUES
+('West Fargo Sports Arena', 'Indoor rink in West Fargo','https://www.west-fargo.k12.nd.us/cms/lib/ND02203445/Centricity/ModuleInstance/7624/dusk%203.jpg', '520 32nd Ave W, West Fargo, ND 58078', 3, FALSE, 'Indoor', 46.834661, -96.908965 );
+
+
+
+INSERT INTO "user" ("username","password", "is_admin")
+VALUES('mleiman0822', 'password', FALSE);
+
+
+CREATE TABLE "favorites" (
+id SERIAL PRIMARY KEY,
+"rink_id" INTEGER REFERENCES "rinks",
+"user_id" INTEGER REFERENCES "user"
+);
+
+SELECT * FROM "user" 
+JOIN "favorites" ON
+"user".id = favorites.user_id
+JOIN  "rinks" ON
+favorites.rink_id = rinks.id;
+
+INSERT INTO "favorites" ("rink_id","user_id")
+VALUES(1, 1);
+
+SELECT "name", "note","image","address","status","favorited","indoor_or_outdoor","longitude","latitude" FROM "rinks" 
+JOIN "favorites" ON
+rinks.id = favorites.rink_id
+JOIN  "user" ON
+favorites.user_id = user_id
+WHERE user_id = 1
+
+DELETE FROM favorites WHERE id = 16;
+
+SELECT "name", "note","image","address","status","favorited","indoor_or_outdoor","longitude","latitude" FROM "rinks" 
+  JOIN "favorites" ON
+  rinks.id = favorites.rink_id
+  WHERE user_id = 4
+
+SELECT "favorites"."id","name", "note","image","address","status","favorited","indoor_or_outdoor","longitude","latitude" FROM "rinks" 
+  JOIN "favorites" ON
+  rinks.id = favorites.rink_id
+  WHERE user_id = 4;
--- src/redux/sagas/rink.saga.js
+++ src/redux/sagas/rink.saga.js
@@ -1,24 +1,6 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-// upload a file (from filestack) to the backend
-/* function* createMapItem(action) {
-  try {
-    // clear any alerts that may be in there already
-    yield put({type: 'CLEAR_ALERT'});
-
-    yield axios.post('/api/map_item', action.payload);
-    // dispatch an alert that the upload was successful
-    yield put({type: 'SET_ALERT', payload: { message: 'Successfully created a map item!', alert: 'alert-success' }});
-    // refresh list of uploads
-    yield put({type: 'FETCH_MAP_ITEMS'});
-  } catch (error) {
-    // dispatch an error that the upload was rejected
-    yield put({type: 'SET_ALERT', payload: { message: 'Error creating map item', alert: 'alert-error' }});
-    console.log('Error getting map items from server:', error);
-  }
-} */
-
 // Refresh the global list of uploads from the database
 function* fetchRinks() {
   try {
@@ -36,7 +18,6 @@ function* fetchRinks() {
 }
 
 function* rinkItemSaga() {
-  /* yield takeLatest('CREATE_MAP_ITEM', createMapItem); */
   yield takeLatest("FETCH_RINKS", fetchRinks);
 }
 
